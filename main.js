ca74e11585470350c3c62541db01e7ac75f948a6
Adelon Medeiros <adelonmedeiros@gmail.com>
2022-02-11 15:54:51 -0300

    update

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const nav = document.querySelector("#header .nav");
+// open and close the menu when the button is clicked
+const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
   element.addEventListener("click", function () {
     nav.classList.toggle("show");
-});
+  });
+}
+
+// close the menu when a item is clicked
+const links = document.querySelectorAll("nav ul li a");
+
+for (const element of links) {
+  element.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
