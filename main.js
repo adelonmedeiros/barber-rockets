6a2c1f8f617ae9fe33efdd9898c6d3442f24da63
Adelon Medeiros <adelonmedeiros@pop-os.localdomain>
2022-03-20 20:31:19 -0300

    project finalized :rocket:

--- main.js
+++ main.js
@@ -17,18 +17,19 @@ for (const element of links) {
   });
 }
 
-const header = document.querySelector("#header"); // get the header
-const navHeight = header.offsetHeight; // get the height of the header
+function changeHeaderWhenScrroll() {
+  const header = document.querySelector("#header"); // get the header
+  const navHeight = header.offsetHeight; // get the height of the header
 
-window.addEventListener("scroll", function () {
-  // add an event listener to the window
   if (this.window.scrollY >= navHeight) {
     // if the window is scrolled more than the height of the header
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll"); // remove the class
   }
-});
+}
+
+
 
 // Testimonials carousel slider swiper
 const swiper = new Swiper(".swiper-container", {
@@ -54,6 +55,23 @@ scrollReveal.reveal(
 #about .image, #about .text,
 #services header, #services .card,
 #testimonials header, #testimonials .testimonials,
-#contact .text, #contact .links,`,
+#contact .text, #contact .links,
+footer .brand, footer .social`,
   { interval: 100 }
 );
+
+// Back to top
+function backToTop() {
+  const backToTopButton = document.querySelector(".back-to-top");
+
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+window.addEventListener("scroll", function () { // add an event listener to the window
+  changeHeaderWhenScrroll()
+  backToTop()
+});
