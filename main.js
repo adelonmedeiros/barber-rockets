f3b0ce301472d549db7857ec13f404e30ee1131d
Adelon Medeiros <adelonmedeiros@pop-os.localdomain>
2022-03-20 21:08:20 -0300

    small adjustments

--- main.js
+++ main.js
@@ -39,6 +39,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: { 
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 });
 
 // ScrollReveal show the elements when the page is scrolled
